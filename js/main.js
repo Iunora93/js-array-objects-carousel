0bb593d6b6309abad96763c8e7b8ec4e7829da7d
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-10 20:15:55 +0100

    Aggiunto ciclo infinito al carosello

--- js/main.js
+++ js/main.js
@@ -23,40 +23,78 @@ const images = [
     }
 ];
 
+/* ---------
+    CONF
+--------- */
 let imageShow = 0;
-const container = document.querySelector(".container");
-images.forEach((element) => { 
-    console.log(element);
-    const listImage = document.getElementById('template-image').content.cloneNode(true);
-    listImage.querySelector(".image-left").src = element.image;
-    container.append(listImage);
-    const containers = document.querySelectorAll(".container-image");
-    
-    containers[imageShow].classList.add("active");
-    
+const imageContainer = document.querySelector(".img-big");
+const imageRight = document.querySelector(".all-img-right");
+/* --------
+    MAIN
+--------- */
+// Clono il template dalla pagina - img left
+const templateImage = document.getElementById('img-big').content.cloneNode(true);
+// Compilo l'html
+templateImage.querySelector('img').src = images[imageShow].image;
+templateImage.querySelector('img').alt = images[imageShow].title;
+templateImage.querySelector('.text-img-big h3').innerHTML = images[imageShow].title;
+templateImage.querySelector('.text-img-big p').innerHTML = images[imageShow].text;
+// Appendo il template al imageContainer
+console.log(templateImage);
+imageContainer.append(templateImage);
 
-});
-
-const prevBtn = document.querySelector(".prev");
-prevBtn.addEventListener("click", function(){
-    document.querySelector(".active").classList.remove("active");
-        
-    if(imageShow === 0){
-        imageShow = imageShow + 1;
-    }else{
-        imageShow = 4;
+// Img right 
+images.forEach((elm, index) => {
+   // Clono il template img right
+   const templateImageRight = document.getElementById("all-img-right").content.cloneNode(true); 
+    // Compilo l'html del template appena clonato
+    // Se l'elemento su cui sto ciclando ha l'indice === imageShow
+    if( index === imageShow ){
+        templateImageRight.querySelector('.img-small').classList.add('active');
     }
-    containers[imageShow].classList.add("active");
+    templateImageRight.querySelector('img').src = elm.image;
+    templateImageRight.querySelector('img').alt = elm.title;
+    // Appendo il template al imageRight
+    imageRight.append(templateImageRight);
 });
-const containers = document.querySelectorAll(".container-image");
-const nextBtn = document.querySelector(".next");
-nextBtn.addEventListener("click", function(){
-    document.querySelector(".active").classList.remove("active");
-       
-    if(imageShow <= 4){
-        imageShow = imageShow + 1;
+
+const allImage = document.querySelectorAll('.img-small');
+// Next image
+const nextBtn = document.querySelector('.next-btn');
+nextBtn.addEventListener('click', function(){
+    // Rimuovo la classe active dalla img-small
+    allImage[imageShow].classList.remove('active');
+    // Incremento l'imageShow
+    if(imageShow < images.length -1 ){
+        imageShow++;
     }else{
         imageShow = 0;
-    }
-    containers[imageShow].classList.add("active");
+    }   
+    // Aggiungo la classe active alla img-small successiva
+    allImage[imageShow].classList.add('active');    
+    // Modifico l'immagine e il testo della img-big
+    imageContainer.querySelector('img').src = images[imageShow].image;
+    imageContainer.querySelector('img').alt = images[imageShow].title;
+    imageContainer.querySelector('.text-img-big h3').innerHTML = images[imageShow].title;
+    imageContainer.querySelector('.text-img-big p').innerHTML = images[imageShow].text;
+});
+
+// prev image
+const prevBtn = document.querySelector('.prev-btn');
+prevBtn.addEventListener('click', function(){
+    // Rimuovo la classe active dalla img-small
+    allImage[imageShow].classList.remove('active');
+    // Incremento l'imageShow
+    if(imageShow > 0 ){
+        imageShow--;
+    }else{
+        imageShow = images.length - 1;
+    }   
+    // Aggiungo la classe active alla img-small successiva
+    allImage[imageShow].classList.add('active');    
+    // Modifico l'immagine e il testo della img-big
+    imageContainer.querySelector('img').src = images[imageShow].image;
+    imageContainer.querySelector('img').alt = images[imageShow].title;
+    imageContainer.querySelector('.text-img-big h3').innerHTML = images[imageShow].title;
+    imageContainer.querySelector('.text-img-big p').innerHTML = images[imageShow].text;
 });
